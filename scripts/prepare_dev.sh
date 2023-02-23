aa9378343f62a553b6115becab3e6cef59d6e99c
Jake Hamilton <jakehamtexas@gmail.com>
2023-02-23 14:22:55 -0600

    make prepare_dev.sh more usable in non-qnr-server repos

--- scripts/prepare_dev.sh
+++ scripts/prepare_dev.sh
@@ -2,14 +2,14 @@
 
 set -e
 
+echo "Copying vim config from $VIM_LOCAL_CONFIG_DIR_PATH."
+cp -rf "$VIM_LOCAL_CONFIG_DIR_PATH" ./.vim || echo "Done (already copied)"
+
 # Only run this in project root
 if [ ! -f ./prepare_dev.sh ]; then
   echo "Not in a monorepo workspace."
   exit 1
 fi
-
-echo "Copying vim config from $VIM_LOCAL_CONFIG_DIR_PATH."
-cp -rf "$VIM_LOCAL_CONFIG_DIR_PATH" ./.vim || echo "Done (already copied)"
 
 echo "Copying cypress secrets from $CYPRESS_SECRETS_PATH."
 cp "$CYPRESS_SECRETS_PATH" ./apps/cypress/ || echo "Done (already copied)"
