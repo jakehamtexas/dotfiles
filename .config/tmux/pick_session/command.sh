6e1789a6b60a5dd669637f9379fa7e73e81afdfb
Jake Hamilton <jakehamtexas@gmail.com>
2023-04-01 12:19:38 -0500

    add pane name to session picker list info

--- .config/tmux/pick_session/command.sh
+++ .config/tmux/pick_session/command.sh
@@ -4,7 +4,7 @@ set -e
 
 . "$TMUX_DIR"/pick_session/fs_utils.sh
 
-if ! picked="$(tmux list-sessions | fzf)"; then
+if ! picked="$(tmux list-windows -a -F '#S|#W|#{pane_current_path}' | fzf | cut -d'|' -f1)" || [ -z "$picked" ]; then
   picked="$(tmux list-clients -F '#S')"
 fi
 
